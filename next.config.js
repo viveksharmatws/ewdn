d5153e8d4b95e0d17a8e6d4d5e4b1fc2f228a816
yash <viveksharma.tws@gmail.com>
2023-12-29 10:51:43 +0530

    removed the error in the next config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-   register: true,
-   scope: '/app',
-   sw: 'service-worker.js',
-  ...
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  scope: "/app",
+  sw: "service-worker.js",
+  // ... other PWA configuration options
+});
 
 module.exports = withPWA({
-  //  next.js config
-})
+  // next.js config
+});
